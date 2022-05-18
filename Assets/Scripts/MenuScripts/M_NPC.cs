8153c3489fcf9640e8b1a50619f7b6463f2643ac
Zogge <oskar@rosenback.eu>
2022-05-18 22:01:23 +0200

    ttt

--- Assets/Scripts/MenuScripts/M_NPC.cs
+++ Assets/Scripts/MenuScripts/M_NPC.cs
@@ -6,10 +6,8 @@ using UnityEngine.UI;
 
 public class M_NPC : MonoBehaviour
 {
-
-
-    public Transform chatBackground;
-    public Transform npcCharacter;
+    public Transform ChatBackGround;
+    public Transform NPCCharacter;
 
     private M_DialogueScript dialogueSystem;
 
@@ -18,36 +16,34 @@ public class M_NPC : MonoBehaviour
     [TextArea(5, 10)]
     public string[] sentences;
 
-    // Start is called before the first frame update
     void Start()
     {
         dialogueSystem = FindObjectOfType<M_DialogueScript>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 pos = Camera.main.WorldToScreenPoint(npcCharacter.position);
-        pos.y += 175;
-        chatBackground.position = pos;
+        Vector3 Pos = Camera.main.WorldToScreenPoint(NPCCharacter.position);
+        Pos.y += 175;
+        ChatBackGround.position = Pos;
     }
 
-    //public void OnTriggerStay(Collider other)
-    //{
-    //    this.gameObject.GetComponent<M_NPC>().enabled = true;
-    //    FindObjectOfType<M_DialogueScript>().EnterRangeOfNPC();
-    //    if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.F))
-    //    {
-    //        this.gameObject.GetComponent<M_NPC>().enabled = true;
-    //        dialogueSystem.name = Name;
-    //        dialogueSystem.dialogueLines = sentences;
-    //        FindObjectOfType<M_DialogueScript>().npcName;
-    //    }
-    //}
+    public void OnTriggerStay(Collider other)
+    {
+        this.gameObject.GetComponent<M_NPC>().enabled = true;
+        FindObjectOfType<M_DialogueScript>().EnterRangeOfNPC();
+        if ((other.gameObject.tag == "Player") && Input.GetKeyDown(KeyCode.F))
+        {
+            this.gameObject.GetComponent<M_NPC>().enabled = true;
+            dialogueSystem.Names = Name;
+            dialogueSystem.dialogueLines = sentences;
+            FindObjectOfType<M_DialogueScript>().NPCName();
+        }
+    }
 
-    //public void OnTriggerExit(Collider other)
-    //{
-    //    FindObjectOfType<M_DialogueScript>().OutOfRange();
-    //    this.gameObject.GetComponent<M_NPC>().enabled = false;
-    //}
+    public void OnTriggerExit()
+    {
+        FindObjectOfType<M_DialogueScript>().OutOfRange();
+        this.gameObject.GetComponent<M_NPC>().enabled = false;
+    }
 }
